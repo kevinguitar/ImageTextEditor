d11b07dfc2c50ffad5afd45b48962f3dd33d82e3
kevin-2017pro <kevin85619@gmail.com>
2018-07-31 15:47:19 +0800

    add: upload progress feature

---
+++ app/src/main/java/com/kevingt/imagetexteditor/ImageEditText.kt
@@ -0,0 +1,193 @@
+package com.kevingt.imagetexteditor
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.RectF
+import android.net.Uri
+import android.support.v4.content.ContextCompat
+import android.support.v7.widget.AppCompatEditText
+import android.text.Editable
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.TextWatcher
+import android.text.style.ImageSpan
+import android.util.AttributeSet
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.SimpleTarget
+import com.bumptech.glide.request.transition.Transition
+
+class ImageEditText @JvmOverloads constructor(context: Context,
+                                              attrs: AttributeSet? = null,
+                                              defStyleAttr: Int = android.R.attr.editTextStyle)
+    : AppCompatEditText(context, attrs, defStyleAttr) {
+
+    companion object {
+        private const val IMAGE_SPAN = "\n \n"
+    }
+
+    private lateinit var rectProgress: RectF
+    private lateinit var paintProgress: Paint
+
+    private val progressStrokeWidth = 16f
+    private val progressWidth = 160
+
+    private var spanHashCode = 0
+    private var image: Bitmap? = null
+    private var imageCopy: Bitmap? = null
+
+    fun initSetting() {
+        paintProgress = Paint()
+        paintProgress.isAntiAlias = true
+        paintProgress.style = Paint.Style.STROKE
+        paintProgress.strokeWidth = progressStrokeWidth
+        paintProgress.color = ContextCompat.getColor(context, R.color.colorAccent)
+        addTextChangedListener(textWatcher)
+    }
+
+    /**
+     * @param uri Pass the uri of photo from intent data
+     */
+    fun insertImage(uri: Uri) {
+        val requestOptions = RequestOptions()
+                .override(800)
+                .fitCenter()
+
+        Glide.with(this)
+                .asBitmap()
+                .load(uri)
+                .apply(requestOptions)
+                .into(object : SimpleTarget<Bitmap>() {
+                    override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                        image = Bitmap.createBitmap(resource)
+                        initUploadView()
+                        setupPlaceHolder()
+                    }
+                })
+
+        //TODO: upload image and update progress
+    }
+
+    /**
+     * @param progress Upload progress between 0-100
+     */
+    fun updateUploadProgress(progress: Int) {
+        if (progress >= 100) {      //upload success
+            replaceImage(image)
+//            image?.recycle()
+//            imageCopy?.recycle()
+            return
+        }
+        if (progress >= 0) {
+            drawProgress(progress)
+        }
+    }
+
+    private fun initUploadView() {
+        rectProgress = RectF()
+        rectProgress.left = (image!!.width / 2).toFloat() - (progressWidth / 2)
+        rectProgress.top = (image!!.height / 2).toFloat() - (progressWidth / 2)
+        rectProgress.right = (image!!.width / 2).toFloat() + (progressWidth / 2)
+        rectProgress.bottom = (image!!.height / 2).toFloat() + (progressWidth / 2)
+    }
+
+    private fun setupPlaceHolder() {
+        imageCopy = Bitmap.createBitmap(image)
+        val canvas = Canvas(imageCopy)
+        canvas.drawARGB(128, 255, 255, 255)
+
+        val imageSpan = ImageSpan(context, imageCopy)
+        val builder = SpannableStringBuilder(text)
+
+        val cursorPosition = selectionStart
+        builder.replace(cursorPosition, selectionEnd, IMAGE_SPAN)
+        builder.setSpan(imageSpan, cursorPosition + 1,
+                cursorPosition + 2, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        text = builder
+        setSelection(cursorPosition + IMAGE_SPAN.length)
+
+        spanHashCode = imageSpan.hashCode()
+    }
+
+    /**
+     * @param progress Upload progress between 0-100
+     */
+    private fun drawProgress(progress: Int) {
+        if (image == null || image!!.isRecycled) {
+            return
+        }
+        imageCopy = Bitmap.createBitmap(image)
+        val canvas = Canvas(imageCopy)
+        val angle = progress * 360 / 100f
+        canvas.drawARGB(128, 255, 255, 255)
+        canvas.drawArc(rectProgress, 270f, angle, false, paintProgress)
+        replaceImage(imageCopy)
+    }
+
+    /**
+     * @param bitmap Update progress or replace to original image when upload done
+     */
+    private fun replaceImage(bitmap: Bitmap?) {
+        if (bitmap == null) {
+            return
+        }
+        val spans = text.getSpans(0, length(), ImageSpan::class.java)
+        if (spans.isEmpty()) {
+            return
+        }
+        val span = ImageSpan(context, bitmap)
+        val builder = SpannableStringBuilder(text)
+        val cursorPosition = selectionStart
+        spans.forEach {
+            if (it.hashCode() == spanHashCode) {
+                val st = text.getSpanStart(it)
+                val en = text.getSpanEnd(it)
+                builder.setSpan(span, st, en, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+            }
+        }
+        text = builder
+        setSelection(cursorPosition)
+    }
+
+    /**
+     * Handle image delete and typing
+     * Always let image occupied one line without any text
+     */
+    private val textWatcher = object : TextWatcher {
+        override fun afterTextChanged(s: Editable?) {}
+
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            if (s.isNullOrEmpty()) {
+                return
+            }
+            val spans = text.getSpans(0, length(), ImageSpan::class.java)
+            if (spans.isEmpty()) {
+                return
+            }
+            removeTextChangedListener(this)
+            spans.forEach {
+                val st = text.getSpanStart(it)
+                val en = text.getSpanEnd(it)
+                if (st < 0 || en < 0) {
+                    return
+                }
+                if (before > 0 && start == en) {                // delete the line below image
+                    text = text.delete(st, en)
+                    setSelection(st)
+                } else if (st > 1 && text[st - 1] != '\n') {    // type before the image
+                    text = text.insert(st, "\n")
+                    setSelection(st)
+                } else if (text[en] != '\n') {                  // type after the image
+                    text = text.insert(en, "\n")
+                    setSelection(en + 2)
+                }
+            }
+            addTextChangedListener(this)
+        }
+    }
+
+}
--- app/src/main/java/com/kevingt/imagetexteditor/MainActivity.kt
+++ app/src/main/java/com/kevingt/imagetexteditor/MainActivity.kt
@@ -1,20 +1,9 @@
 package com.kevingt.imagetexteditor
 
 import android.content.Intent
-import android.graphics.Bitmap
-import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.text.Editable
-import android.text.Spannable
-import android.text.SpannableStringBuilder
-import android.text.TextWatcher
-import android.text.style.ImageSpan
 import android.view.View
-import com.bumptech.glide.Glide
-import com.bumptech.glide.request.RequestOptions
-import com.bumptech.glide.request.target.SimpleTarget
-import com.bumptech.glide.request.transition.Transition
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
@@ -22,82 +11,23 @@ class MainActivity : AppCompatActivity() {
 
     companion object {
         private const val REQ_CHOOSE_IMAGE = 1
-        private const val IMAGE_SPAN = "\n \n"
     }
 
-    private val imageSpanHashStack: Stack<Int> = Stack()
-
-    private val textWatcher = object : TextWatcher {
-        override fun afterTextChanged(s: Editable?) {}
-
-        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-
-        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-            if (s.isNullOrEmpty()) {
-                return
-            }
-            val spans = et_main.text.getSpans(0, et_main.length(), ImageSpan::class.java)
-            if (spans.isEmpty()) {
-                return
-            }
-            et_main.removeTextChangedListener(this)
-            spans.forEach {
-                val st = et_main.text.getSpanStart(it)
-                val en = et_main.text.getSpanEnd(it)
-                if (before > 0 && start == en) {                        // delete the line below image
-                    et_main.text = et_main.text.delete(st, en)
-                    et_main.setSelection(st)
-                } else if (st > 1 && et_main.text[st - 1] != '\n') {    // type before the image
-                    et_main.text = et_main.text.insert(st, "\n")
-                    et_main.setSelection(st)
-                } else if (et_main.text[en] != '\n') {                  // type after the image
-                    et_main.text = et_main.text.insert(en, "\n")
-                    et_main.setSelection(en + 2)
-                }
-            }
-            et_main.addTextChangedListener(this)
-        }
-    }
+    private val r = Random()
+    private var uploadProgress = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        et_main.addTextChangedListener(textWatcher)
+        et_main.initSetting()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == REQ_CHOOSE_IMAGE && data != null) {
-            parseUri(data.data)
+            uploadProgress = 0
+            et_main.insertImage(data.data)
         }
-    }
-
-    private fun parseUri(uri: Uri) {
-        val requestOptions = RequestOptions()
-                .override(800)
-                .fitCenter()
-
-        Glide.with(this)
-                .asBitmap()
-                .load(uri)
-                .apply(requestOptions)
-                .into(object : SimpleTarget<Bitmap>() {
-                    override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
-                        insertImage(resource)
-                    }
-                })
-    }
-
-    private fun insertImage(bitmap: Bitmap) {
-        val imageSpan = ImageSpan(this, bitmap)
-        val builder = SpannableStringBuilder(et_main.text)
-
-        val cursorPosition = et_main.selectionStart
-        builder.replace(cursorPosition, et_main.selectionEnd, IMAGE_SPAN)
-        builder.setSpan(imageSpan, cursorPosition + 1,
-                cursorPosition + 2, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
-        et_main.text = builder
-        et_main.setSelection(cursorPosition + IMAGE_SPAN.length)
     }
 
     fun chooseImage(v: View) {
@@ -108,22 +38,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun takePhoto(v: View) {
-        val drawable = getDrawable(R.drawable.image_progress_bar)
-        drawable.setBounds(0, 0, 800, 800)
-        imageSpanHashStack.add(ImageSpanUtil.setPlaceHolderSpan(et_main, drawable))
-
-        val requestOptions = RequestOptions()
-                .override(800)
-                .fitCenter()
-
-        Glide.with(this)
-                .asBitmap()
-                .load("https://c2.staticflickr.com/8/7151/6760135001_14c59a1490_o.jpg") // 8000 x 8000
-                .apply(requestOptions)
-                .into(object : SimpleTarget<Bitmap>() {
-                    override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
-                        ImageSpanUtil.replaceLoadedImage(et_main, resource, imageSpanHashStack.pop())
-                    }
-                })
+        uploadProgress += 10 + r.nextInt(20)
+        et_main.updateUploadProgress(uploadProgress)
     }
 }
